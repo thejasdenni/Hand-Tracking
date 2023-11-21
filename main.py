2d6c9676da918bb62055d9f915c0db9f39a04f0c
thejasdenni <thejasdenni@gmail.com>
2023-11-22 00:03:48 +0530

    Make UDP Protocol and Build Server

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2
 from cvzone.HandTrackingModule import HandDetector
+import socket
 
 #Parameters
 width, height = 1280,720
@@ -12,6 +13,10 @@ cap.set(4,height)
 
 #Hand_Detection
 detector=HandDetector(maxHands=1,detectionCon=0.8)
+
+#Communication
+sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+serverAddressPort = ("127.0.0.1",5052)
 
 
 while True:
@@ -28,10 +33,11 @@ while True:
         hand =  hands[0]
         #Get the Landmark list
         lmList = hand['lmList']
-        print(lmList)
+        #print(lmList)
         for lm in lmList:
             data.extend([lm[0],height - lm[1],lm[2]])
-        print(data)
+        #print(data)
+        sock.sendto(str.encode(str(data)),serverAddressPort)
 
     cv2.imshow("Image",img)
     cv2.waitKey(1)
